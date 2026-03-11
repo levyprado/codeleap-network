d57f68543e0c8111faa99b186d76b30c86201cf4
Levy Prado <levyprado28@gmail.com>
2026-03-10 21:21:36 -0400

    refactor: remove redundant main wrapper in signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,7 +5,7 @@ import Label from '@/components/ui/label'
 
 export default function SignupPage() {
   return (
-    <main className='grid min-h-dvh place-items-center px-4'>
+    <>
       <Card title='Welcome to CodeLeap Network!'>
         <form className='flex flex-col gap-4'>
           <div className='flex flex-col gap-2.5'>
@@ -16,6 +16,6 @@ export default function SignupPage() {
           <Button className='mt-2'>Enter</Button>
         </form>
       </Card>
-    </main>
+    </>
   )
 }
