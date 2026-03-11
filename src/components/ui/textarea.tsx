53dca8970ad6fc6756bd095df455fc78479ce887
Levy Prado <levyprado28@gmail.com>
2026-03-10 23:09:12 -0400

    style: add max-height and transition colors to textarea

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ export default function Textarea({
     <textarea
       data-slot='textarea'
       className={cn(
-        'flex field-sizing-content min-h-16 w-full rounded-lg border border-input bg-transparent px-2.5 py-2 shadow-sm transition-all outline-none placeholder:text-muted-foreground focus-visible:border-accent focus-visible:ring-2 focus-visible:ring-accent/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 dark:bg-input/30 dark:disabled:bg-input/80',
+        'flex field-sizing-content max-h-32 min-h-16 w-full rounded-lg border border-input bg-transparent px-2.5 py-2 shadow-sm transition-colors outline-none placeholder:text-muted-foreground focus-visible:border-accent focus-visible:ring-2 focus-visible:ring-accent/50 disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 dark:bg-input/30 dark:disabled:bg-input/80',
         className,
       )}
       {...props}
