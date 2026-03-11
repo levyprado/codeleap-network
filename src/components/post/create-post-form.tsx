fffc2703b2c270b8928b8d6793018c60cdc02d3f
Levy Prado <levyprado28@gmail.com>
2026-03-10 23:09:56 -0400

    feat: add image upload button to create post form

--- src/components/post/create-post-form.tsx
+++ src/components/post/create-post-form.tsx
@@ -1,17 +1,19 @@
+import { ImageIcon } from '@hugeicons/core-free-icons'
 import Button from '../ui/button'
+import IconButton from '../ui/icon-button'
 import Input from '../ui/input'
 import Label from '../ui/label'
 import Textarea from '../ui/textarea'
 
 export default function CreatePostForm() {
   return (
-    <div className='flex shrink-0 flex-col gap-5'>
+    <div className='flex shrink-0 flex-col gap-4'>
       <h2 className='text-xl font-semibold text-balance md:text-3xl'>
         What&apos;s on your mind?
       </h2>
 
-      <form className='flex flex-col gap-5'>
-        <div className='flex flex-col gap-2.5'>
+      <form className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-2'>
           <Label>Title</Label>
           <Input placeholder='How to join CodeLeap' />
         </div>
@@ -20,7 +22,11 @@ export default function CreatePostForm() {
           <Textarea placeholder='Create a good project in the challenge and...' />
         </div>
 
-        <Button className='ml-auto px-8'>Create Post</Button>
+        <div className='flex items-center justify-between pt-2'>
+          <IconButton icon={ImageIcon} />
+
+          <Button className='ml-auto px-8'>Create Post</Button>
+        </div>
       </form>
     </div>
   )
