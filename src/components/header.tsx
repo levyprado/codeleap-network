e7c72d839ca58fe6381d3c22678fac44156de6db
Levy Prado <levyprado28@gmail.com>
2026-03-11 12:16:53 -0400

    refactor: use site config for application name in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
+import { siteConfig } from '@/lib/constants'
 import UserMenu from './user-menu'
 
 export default function Header() {
   return (
     <header className='flex h-16 items-center justify-between gap-3 bg-brand px-6'>
       <h1 className='text-xl font-bold text-balance text-white md:text-2xl'>
-        CodeLeap Network
+        {siteConfig.name}
       </h1>
 
       <UserMenu />
