770bf057a7b52d4f26d493d7f619ee1a74d01aa1
Levy Prado <levyprado28@gmail.com>
2026-03-12 15:32:28 -0400

    feat: implement update post mutation

--- src/features/post/mutations.ts
+++ src/features/post/mutations.ts
@@ -1,12 +1,25 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { postsApi } from './api'
 import { postKeys } from './queries'
+import { CreatePostPayload } from './types'
 
 export const useCreatePost = () => {
   const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: postsApi.createPost,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: postKeys.all })
+    },
+  })
+}
+
+export const useUpdatePost = (id: number) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (data: Partial<CreatePostPayload>) =>
+      postsApi.updatePost(id, data),
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: postKeys.all })
     },
