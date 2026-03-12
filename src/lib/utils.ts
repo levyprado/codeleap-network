9891c5f43bf685e3c547357bdf01d5fe762859a5
Levy Prado <levyprado28@gmail.com>
2026-03-12 00:34:56 -0400

    feat: implement formatPostDate util for datetime formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,33 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPostDate(createdAt: string): string {
+  const now = new Date()
+  const postDate = new Date(createdAt)
+  const secondsAgo = Math.round((now.getTime() - postDate.getTime()) / 1000)
+
+  const MINUTE = 60
+  const HOUR = MINUTE * 60
+  const DAY = HOUR * 24
+
+  if (secondsAgo < DAY) {
+    let dateStr = ''
+    if (secondsAgo < MINUTE) {
+      dateStr += `${secondsAgo}s`
+    } else if (secondsAgo < HOUR) {
+      dateStr += `${Math.floor(secondsAgo / MINUTE)}m`
+    } else {
+      dateStr += `${Math.floor(secondsAgo / HOUR)}h`
+    }
+    return `${dateStr} ago`
+  }
+
+  const isDifferentYear = now.getFullYear() !== postDate.getFullYear()
+
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: isDifferentYear ? 'numeric' : undefined,
+  }).format(postDate)
+}
