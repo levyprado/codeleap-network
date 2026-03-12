42e4c482204716e346f82ee4e13346a524af2025
Levy Prado <levyprado28@gmail.com>
2026-03-11 23:22:04 -0400

    refactor: update API with typed responses and correct env var

--- src/features/post/api.ts
+++ src/features/post/api.ts
@@ -1,16 +1,20 @@
 import axios from 'axios'
-import { CreatePostPayload } from './types'
+import type { CreatePostPayload, Post } from './types'
 
-const API_URL = process.env.API_BASE_URL
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 const api = axios.create({
   baseURL: API_URL,
 })
 
 export const postsApi = {
-  getPosts: () => api.get(''),
-  createPost: (data: CreatePostPayload) => api.post('', data),
+  getPosts: () => api.get<{ results: Post[] }>('').then((res) => res.data),
+
+  createPost: (data: CreatePostPayload) =>
+    api.post<Post>('', data).then((res) => res.data),
+
   updatePost: (id: number, data: Partial<CreatePostPayload>) =>
     api.patch(`${id}/`, data),
+
   deletePost: (id: number) => api.delete(`${id}/`),
 }
