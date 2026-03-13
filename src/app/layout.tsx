a70cd50e460d9c292a2ebf730d04039f3b5da220
Levy Prado <levyprado28@gmail.com>
2026-03-12 21:34:23 -0400

    feat: configure sonner toaster component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import QueryProvider from '@/components/query-provider'
+import Icon from '@/components/ui/icon'
 import AuthGuard from '@/features/auth/components/auth-guard'
 import { siteConfig } from '@/lib/constants'
+import {
+  CancelCircleIcon,
+  CheckmarkCircle01Icon,
+} from '@hugeicons/core-free-icons'
 import type { Metadata } from 'next'
 import { Inter_Tight } from 'next/font/google'
+import { Toaster } from 'sonner'
 import './globals.css'
 
 const inter = Inter_Tight({
@@ -33,6 +39,22 @@ export default function RootLayout({
             <AuthGuard>{children}</AuthGuard>
           </QueryProvider>
         </main>
+        <Toaster
+          position='bottom-center'
+          visibleToasts={1}
+          offset={36}
+          mobileOffset={32}
+          duration={3000}
+          icons={{
+            success: <Icon icon={CheckmarkCircle01Icon} size={20} />,
+            error: <Icon icon={CancelCircleIcon} size={20} />,
+          }}
+          className='flex justify-center'
+          toastOptions={{
+            className:
+              'font-sans! bg-card! px-5! text-foreground! border-2! border-border! text-base! shadow-lg!',
+          }}
+        />
       </body>
     </html>
   )
